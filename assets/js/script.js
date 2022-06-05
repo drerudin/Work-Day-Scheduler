8e8893618659efe8207131ed08c388cd44db2fae
Andrey Rudinskiy <drerudin@gmail.com>
2022-06-04 21:33:27 -0700

    current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-var currentDay = moment().format("dddd, MMMM Do");
 
+// added current date to the top of the page
+var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
+
+function saveButton = 
